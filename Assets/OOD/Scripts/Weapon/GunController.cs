2ef0a450c0af6fd0f01474566688ac655199e1bf
fpil <pilfrederik@gmail.com>
2023-03-17 12:25:17 +0100

    Added simple shooting system to OOD

---
+++ Assets/OOD/Scripts/Weapon/GunController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace OOD.Scripts.Weapon
+{
+    public class GunController : MonoBehaviour
+    {
+        public GameObject bulletPrefab;
+        public Transform muzzleTransform; 
+   
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            GameObject bullet = Instantiate(bulletPrefab, muzzleTransform.position, muzzleTransform.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.AddForce(muzzleTransform.forward * 2000f);
+        }
+    }
+}

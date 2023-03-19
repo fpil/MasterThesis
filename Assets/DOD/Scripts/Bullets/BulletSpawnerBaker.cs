9d3717ae479e630f772672ee5d771f38e8c15422
fpil <pilfrederik@gmail.com>
2023-03-19 23:11:57 +0100

    Added initial bullet spawn system to DOD

---
+++ Assets/DOD/Scripts/Bullets/BulletSpawnerBaker.cs
@@ -0,0 +1,30 @@
+using DOD.Scripts.Bullets;
+using Unity.Entities;
+using UnityEngine;
+
+namespace DOD.Scripts.Bullets
+{
+    public class BulletSpawnerBaker : MonoBehaviour
+    {
+        public GameObject spawnPosition;
+        public Transform bulletPrefab; 
+    }
+}
+
+public struct BulletSpawnPositionComponent : IComponentData
+{
+    public Entity BulletPrefab;
+    public Entity BulletSpawn;
+}
+
+public class BulletBaker : Baker<BulletSpawnerBaker>
+{
+    public override void Bake(BulletSpawnerBaker authoring)
+    {
+        AddComponent(new BulletSpawnPositionComponent
+        {
+            BulletPrefab = GetEntity(authoring.bulletPrefab),
+            BulletSpawn = GetEntity(authoring.spawnPosition),
+        } );
+    }
+}
--- Assets/DOD/Scripts/Bullets/BulletSystem.cs
+++ Assets/DOD/Scripts/Bullets/BulletSystem.cs
@@ -2,12 +2,14 @@ using DOD.Scripts.Bullets;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 public partial struct BulletSystem : ISystem
 {
+    ComponentLookup<WorldTransform> m_WorldTransformLookup;
     public void OnCreate(ref SystemState state)
     {
-        
+        m_WorldTransformLookup = state.GetComponentLookup<WorldTransform>(true);
     }
 
     public void OnDestroy(ref SystemState state)
@@ -16,19 +18,24 @@ public partial struct BulletSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        foreach (RefRW<BulletSpawnerPositionComponent> spawner in SystemAPI.Query<RefRW<BulletSpawnerPositionComponent>>())
+        m_WorldTransformLookup.Update(ref state);
+        if (Input.GetButtonDown("Fire1"))
         {
-            BulletSpawner(ref state, spawner);
+            foreach (RefRW<BulletSpawnPositionComponent> spawner in SystemAPI.Query<RefRW<BulletSpawnPositionComponent>>())
+            {
+                BulletSpawner(ref state, spawner, m_WorldTransformLookup);
+            }
         }
+
+        
     }
     
-    private void BulletSpawner(ref SystemState state, RefRW<BulletSpawnerPositionComponent> spawner)
+    private void BulletSpawner(ref SystemState state, RefRW<BulletSpawnPositionComponent> spawner,
+        ComponentLookup<WorldTransform> WorldTransformLookup)
     {
-        // Spawns a new entity and positions it at the spawner.
-            Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
-            state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(new float3(0,0,0)));
-        
-            // Resets the next spawn time.
-            // spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+        var spawnLocalToWorld = WorldTransformLookup[spawner.ValueRO.BulletSpawn];
+        var bulletTransform = LocalTransform.FromPosition(spawnLocalToWorld.Position);
+        Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.BulletPrefab);
+        state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(bulletTransform._Position));
     }
 }

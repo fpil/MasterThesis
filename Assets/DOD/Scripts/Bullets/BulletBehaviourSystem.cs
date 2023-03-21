c74c1a25ab8b66034759a31841eb1989eab6bad7
fpil <pilfrederik@gmail.com>
2023-03-21 11:09:42 +0100

    Made structural change to so that old bullets will not be queried

--- Assets/DOD/Scripts/Bullets/BulletBehaviourSystem.cs
+++ Assets/DOD/Scripts/Bullets/BulletBehaviourSystem.cs
@@ -4,7 +4,9 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using Unity.Transforms;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DOD.Scripts.Bullets
 {
@@ -21,45 +23,50 @@ namespace DOD.Scripts.Bullets
 
         public void OnUpdate(ref SystemState state)
         {
+            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+
             var deltaTime = state.WorldUnmanaged.Time.DeltaTime;
             
             //Todo --> this does not really work because the job is never stalled
             var muzzleGameObject = GameObject.Find("MuzzleGameObject");
             var vector3 = muzzleGameObject.transform.forward;
 
-
             var bulletFireJob = new BulletFireJob()
             {
                 deltaTime = deltaTime,
-                vector3 = vector3
+                vector3 = vector3,
+                Ecb = ecb
             };    
             //Need to be paralle or race condition
-            state.Dependency = bulletFireJob.ScheduleParallel(state.Dependency);
+            // state.Dependency = bulletFireJob.ScheduleParallel(state.Dependency);
 
-            // bulletFireJob.Run();
+            bulletFireJob.Run();
         }
-        
+
         [WithAll(typeof(BulletTag))]
         public partial struct BulletFireJob : IJobEntity
         {
             public float deltaTime;
             public Vector3 vector3 { get; set; }
+            public EntityCommandBuffer Ecb;
 
-            public void Execute(ref LocalTransform localTransform, ref PhysicsVelocity velocity, ref BulletFired fired)
+            public void Execute(in Entity entity, ref PhysicsVelocity velocity, ref BulletFired fired)
             {
                 //Saves the original fire direction
                 if (fired._hasFired == 0)
                 {
                     fired._hasFired = 1;
                     fired.fireDirection = vector3;
+                    //Only adds velocity one time
+                    velocity.Linear += new float3(fired.fireDirection * 5000)*deltaTime;
+
+                    //This is expensive structural change
+                    Ecb.RemoveComponent<BulletFired>(entity);
+                    
                 }
-                velocity.Linear += new float3(fired.fireDirection * 20)*deltaTime;
-
-
-                
             }
         }
-       
     }
 }
     

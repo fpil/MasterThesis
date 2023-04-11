d6cea504c28e02f301c6c62b5c684cb19a8fa191
fpil <pilfrederik@gmail.com>
2023-04-11 14:54:42 +0200

    implemented the loot behavior

--- Assets/DOD/Scripts/Loot/LootBehaviorSystem.cs
+++ Assets/DOD/Scripts/Loot/LootBehaviorSystem.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
 
 public partial struct LootBehaviorSystem : ISystem
 {
+    private Random generator;
+
     public void OnCreate(ref SystemState state)
     {
     }
@@ -23,23 +28,29 @@ public partial struct LootBehaviorSystem : ISystem
         var deltaTime = state.WorldUnmanaged.Time.DeltaTime;
         var elaspedTime = state.WorldUnmanaged.Time.ElapsedTime;
         var collisionWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().CollisionWorld;
+        generator = new Random((uint) UnityEngine.Random.Range(-10000, 10000));
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        EntityCommandBuffer ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
 
 
-        var lootMovementJob = new LootMovementJob
+
+        var lootMovementJob = new LootBehaviorJob
         {
             deltaTime = deltaTime,
             elapsedTime = elaspedTime,
             world = collisionWorld, 
             player = SystemAPI.GetComponentLookup<PlayerTagComponent>(true),
-            // machinegunLoot = SystemAPI.GetComponentLookup<MachinegunLootTag>(true),
             shotgunLoot = SystemAPI.GetComponentLookup<ShotgunLootTag>(true),
+            bulletController = SystemAPI.GetSingletonRW<Ammo>(), 
+            generator = generator, 
+            ecb = ecb.AsParallelWriter()
         };
         state.Dependency = lootMovementJob.ScheduleParallel(state.Dependency);
         state.Dependency.Complete();
     }
     
     [BurstCompile]
-    public partial struct LootMovementJob : IJobEntity
+    public partial struct LootBehaviorJob : IJobEntity
     {
         public float deltaTime { get; set; }
         public double elapsedTime { get; set; }
@@ -47,11 +58,13 @@ public partial struct LootBehaviorSystem : ISystem
         private const float separationRadius = 0.50f;
         [field: ReadOnly] public CollisionWorld world { get; set; }
         [field: ReadOnly] public ComponentLookup<PlayerTagComponent> player { get; set; }
-        // [field: ReadOnly] public ComponentLookup<MachinegunLootTag> machinegunLoot { get; set; }
         [field: ReadOnly] public ComponentLookup<ShotgunLootTag> shotgunLoot { get; set; }
+        [field: NativeDisableUnsafePtrRestriction] public RefRW<Ammo> bulletController { get; set; }
+        public Random generator { get; set; }
+        public EntityCommandBuffer.ParallelWriter ecb { get; set; }
 
 
-        void Execute(in Entity entity, in LootTag lootTag, ref LocalTransform localTransform)
+        void Execute([ChunkIndexInQuery] int chunkIndex,in Entity entity, in LootTag lootTag, ref LocalTransform localTransform)
         {
             localTransform.Rotation = math.mul(localTransform.Rotation, quaternion.RotateY(1.0f*deltaTime));
             
@@ -64,10 +77,13 @@ public partial struct LootBehaviorSystem : ISystem
                     {
                         if (shotgunLoot.HasComponent(entity))
                         {
+                            bulletController.ValueRW.ShotgunAmmo += generator.NextInt(10, 30);
                         }
                         else
                         {
+                            bulletController.ValueRW.MachineGunAmmo += generator.NextInt(30, 100);
                         }
+                        ecb.DestroyEntity(chunkIndex,entity);
                     }
                 }
             }
